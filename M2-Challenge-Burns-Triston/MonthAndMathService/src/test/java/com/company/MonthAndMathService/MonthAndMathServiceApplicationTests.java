3a265cb22037ec8dafab518317c2a940faa1489b
Triston Burns <tristonburns97@gmail.com>
2022-03-01 15:57:07 -0700

    Push MonthAndMathService project to repository

---
+++ M2-Challenge-Burns-Triston/MonthAndMathService/src/test/java/com/company/MonthAndMathService/MonthAndMathServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.MonthAndMathService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthAndMathServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
